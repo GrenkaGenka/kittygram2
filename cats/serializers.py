9d52e5b2cf2a75e5f8a6502bb34c73ea629cf62c
Genka <Gennadiusisus@gmil.com>
2024-12-20 12:41:38 +0700

    last

--- cats/serializers.py
+++ cats/serializers.py
@@ -27,11 +27,25 @@ class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(many=True, required=False)
     color = serializers.ChoiceField(choices=CHOICES)
     age = serializers.SerializerMethodField()
+    owner = serializers.HiddenField(default=serializers.CurrentUserDefault())
     
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'achievements', 'owner',
-                  'age')
+        fields = ('id', 'name', 'color', 'birth_year',
+                      'achievements', 'owner', 'age')
+    
+    def validate(self, data):
+        if data['color'] == data['name']:
+            raise serializers.ValidationError(
+                'Имя не может совпадать с цветом!')
+        return data 
+
+    def validate_birth_year(self, value):
+        year = dt.date.today().year
+        if not (year - 40 < value <= year):
+            raise serializers.ValidationError('Проверьте год рождения!')
+        return value
+
 
     def get_age(self, obj):
         return dt.datetime.now().year - obj.birth_year
